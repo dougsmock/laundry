3c648079209aecd22c12197c1d0af25f575173db
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-04 16:32:31 -0400

    Add 2 functions

--- LaundryCycle.rb
+++ LaundryCycle.rb
@@ -3,6 +3,15 @@ def wear(smell)
   sleep 2
 end
 
+def dirty
+  1 == [1, 2].choose
+end
+
+def treat_stain
+  sample :bd_boom
+  sleep 1
+end
+
 def wash(temperature)
   play temperature
   sleep 0.5
@@ -15,6 +24,7 @@ end
 
 5.times do
   wear(sample :drum_cymbal_hard, rate: 1.25)
+  treat_stain if dirty
   wash(60)
   dry(sample :bass_woodsy_c)
 end
